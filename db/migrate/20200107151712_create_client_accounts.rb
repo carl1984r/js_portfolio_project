4a4c96789b72c6434f9e0fb27b83ed360ea3f6d1
carl1984r <carl.cc@gmail.com>
2020-01-08 08:35:10 -0600

    Updated + migrated dbase.

--- db/migrate/20200107151712_create_client_accounts.rb
+++ db/migrate/20200107151712_create_client_accounts.rb
@@ -1,8 +1,8 @@
 class CreateClientAccounts < ActiveRecord::Migration[5.2]
   def change
     create_table :client_accounts do |t|
-      t.integer :client_id
-      t.integer :account_id
+      t.references :client, foreign_key: true
+      t.references :account, foreign_key: true
 
       t.timestamps
     end
