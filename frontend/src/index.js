db6aa6f831d51b80d3dad94d4e3002fa1c5c16bb
carl1984r <carl.cc@gmail.com>
2020-01-15 03:54:17 -0600

    Updated styling.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,36 @@
 const BASE_URL = "http://localhost:3000";
 const CLIENTS_URL = `${BASE_URL}/clients`;
 
-$(document).ready(function() {
-  console.log('Hello World');
-  fetch(CLIENTS_URL).then(resp => resp.json())
-    .then(json => console.log(json));
-});
+//$(document).ready(function() {
+//  console.log('Hello World');
+//  fetch(CLIENTS_URL).then(resp => resp.json())
+//    .then(json => console.log(json));
+//});
+
+function fetchClients() {
+  return fetch(CLIENTS_URL)
+
+  .then(function(response) {
+      return response.json();
+    })
+
+    .then(function(json) {
+      renderClients(json);
+    });
+}
+
+function renderClients(json) {
+  const td = document.querySelector('div.item.sidebar table tbody tr td')
+  const thead = document.querySelector('div.item.sidebar table thead')
+  const hr = document.createElement('hr')
+  thead.appendChild(hr)
+  json.forEach(client => {
+    const li = document.createElement('li')
+    li.innerHTML = `${client.name}`
+    td.appendChild(li)
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchClients()
+})
