9ab796f2bb9e7ecb5a38e4713e85c1fdfb86484b
carl1984r <carl.cc@gmail.com>
2020-01-29 16:42:29 -0600

    Updated #table2, renderClient(s), fetchClient(s).

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,30 +10,79 @@ const CLIENT_ACCOUNTS_URL = `${BASE_URL}/client_accounts`;
 //});
 
 function fetchClients() {
-  fetch(CLIENTS_URL).then(resp => resp.json()).then(json => renderClients(json.data));
+  fetch(CLIENTS_URL).then(resp => resp.json()).then(json => renderClients(json))
   }
 
 function fetchClient(id) {
-  let url = `${CLIENT_ACCOUNTS_URL}/${id}`;
-  fetch(url).then(resp => resp.json()).then(json => renderClient(json.data));
+  let url = `${CLIENT_ACCOUNTS_URL}/${id}`
+  fetch(url).then(resp => resp.json()).then(json => renderClient(json))
   }
 
 function renderClient(json) {
-  const td = document.querySelector('div.item.content-1 table tbody tr td')
-  const thead = document.querySelector('div.item.content-1 table#table2 caption')
-  const hr = document.createElement('hr')
-  thead.appendChild(hr)
-}
+  const caption = document.querySelector('div.item.content-1 table#table2 caption')
+  const table = document.querySelector('div.item.content-1 table#table2 tbody')
+  const th = document.querySelectorAll("th")
+    th[0].textContent = "Account name"
+    th[1].textContent = "Utilization"
+    th[2].textContent = "Account number"
+
+  let hr1 = document.createElement('hr')
+    caption.textContent = "Client account(s)"
+    caption.appendChild(hr1)
+
+  let hr
+    for(let i = 0; i < 3; i++) {
+      hr = document.createElement('hr')
+      th[i].appendChild(hr)
+    }
+
+    json.data.slice().reverse().forEach(client_account => {
+      let link0 = document.createTextNode(`${client_account["attributes"].account["name"]}`)
+      let link1 = document.createTextNode(`${client_account["attributes"].account["utilization"]}`)
+      let link2 = document.createTextNode(`${client_account["attributes"].account["number"]}`)
+
+      //for(let i = 0; i < 3; i++) {
+      //  let a + i = document.createElement('a')
+      //}
+
+      let a0 = document.createElement('a')
+      let a1 = document.createElement('a')
+      let a2 = document.createElement('a')
+
+      a0.appendChild(link0)
+      a1.appendChild(link1)
+      a2.appendChild(link2)
+
+      let row = table.insertRow(1)
+
+      let cell0 = row.insertCell(0)
+      let cell1 = row.insertCell(1)
+      let cell2 = row.insertCell(2)
+
+      cell0.appendChild(a0)
+      cell1.appendChild(a1)
+      cell2.appendChild(a2)
+
+      cell0.style.textAlign = "center"
+      cell1.style.textAlign = "center"
+      cell2.style.textAlign = "center"
+      
+      //a.href = `${CLIENT_ACCOUNTS_URL}/${client.id}`
+      //a.onclick = function() {fetchClient(`${client["id]"]}`)}
+      //li.appendChild(a)
+      //td.appendChild(li)
+    })
+  }
 
 function renderClients(json) {
   const td = document.querySelector('div.item.sidebar table#table1 tr td')
   const caption = document.querySelector('div.item.sidebar table#table1 caption')
   const hr = document.createElement('hr')
   caption.appendChild(hr)
-  json.forEach(client => {
+  json.data.forEach(client => {
     let a = document.createElement('a')
     let li = document.createElement('li')
-    let link = document.createTextNode(`${client["attributes"]["name"]}`);
+    let link = document.createTextNode(`${client["attributes"]["name"]}`)
     a.appendChild(link)
     //a.title = `${client.attributes}`
     //a.href = `${CLIENT_ACCOUNTS_URL}/${client.id}`
