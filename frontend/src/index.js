1f44d6673ae95e72b24744ddcbb9925736831ced
carl1984r <carl.cc@gmail.com>
2020-02-10 03:00:55 -0600

    Updated Balance/Transaction history + renderAccountTransacts, added table3 width.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -24,16 +24,59 @@ function fetchAccountTransacts(id) {
   fetch(url).then(resp => resp.json()).then(json => renderAccountTransacts(json))
 }
 
+const caption1 = document.querySelector('div.item.content-1 table#table2 caption')
+const caption2 = document.querySelector('div.item.content-2 table#table3 caption')
+const th = document.querySelectorAll("th")
+
 function renderAccountTransacts(json) {
+  let table = document.querySelector('div.item.content-2 table#table3 tbody')
+  let hr1 = document.createElement('hr')
+    caption2.textContent = "Account Balance/Transaction history"
+    caption2.appendChild(hr1)
 
+  th[3].textContent = "Date"
+  th[4].textContent = "Type"
+  th[5].textContent = "Description"
+  th[6].textContent = "Amount"
+  th[7].textContent = "Balance"
 
+  let hr
+    for(let i = 3; i < 8; i++) {
+      hr = document.createElement('hr')
+      th[i].appendChild(hr)
+    }
+
+    json.included.slice().reverse().forEach(account_transacts => {
+      let text0 = document.createTextNode(`${account_transacts["attributes"]["date"]}`)
+      let text1 = document.createTextNode(`${account_transacts["attributes"]["transact_type"]}`)
+      let text2 = document.createTextNode(`${account_transacts["attributes"]["description"]}`)
+      let text3 = document.createTextNode(`${account_transacts["attributes"]["amount"]}`)
+      let text4 = document.createTextNode(`${account_transacts["attributes"]["run"]}`)
+
+      let row = table.insertRow(1)
+
+      let cell0 = row.insertCell(0)
+      let cell1 = row.insertCell(1)
+      let cell2 = row.insertCell(2)
+      let cell3 = row.insertCell(3)
+      let cell4 = row.insertCell(4)
+
+      cell0.appendChild(text0)
+      cell1.appendChild(text1)
+      cell2.appendChild(text2)
+      cell3.appendChild(text3)
+      cell4.appendChild(text4)
+
+      cell0.style.textAlign = "center"
+      cell1.style.textAlign = "center"
+      cell2.style.textAlign = "center"
+      cell3.style.textAlign = "center"
+      cell4.style.textAlign = "center"
+    })
 }
 
 function renderClientAccounts(json) {
-  const caption = document.querySelector('div.item.content-1 table#table2 caption')
-  const table = document.querySelector('div.item.content-1 table#table2 tbody')
-  const table2 = document.querySelector('div.item.content-2')
-  const th = document.querySelectorAll("th")
+  let table = document.querySelector('div.item.content-1 table#table2 tbody')
 
   let i
     for (i = table.rows.length - 1; i > 0; i--) {
@@ -44,11 +87,11 @@ function renderClientAccounts(json) {
   th[1].textContent = "Utilization"
   th[2].textContent = "Account number"
 
-  table2.textContent = "Select an account to view Balance/Transaction history"
+  caption2.textContent = "Select an account to view Balance/Transaction history"
 
   let hr1 = document.createElement('hr')
-    caption.textContent = "Client account(s)"
-    caption.appendChild(hr1)
+    caption1.textContent = "Client account(s)"
+    caption1.appendChild(hr1)
 
   let hr
     for(let i = 0; i < 3; i++) {
@@ -69,11 +112,11 @@ function renderClientAccounts(json) {
       a1.appendChild(link1)
       a2.appendChild(link2)
 
-      a0.href = `${ACCOUNT_TRANSACTS_URL}/${client_account.id}`
+      a0.onclick = function() {fetchAccountTransacts(`${client_account["id"]}`)}
+      a1.onclick = function() {fetchAccountTransacts(`${client_account["id"]}`)}
+      a2.onclick = function() {fetchAccountTransacts(`${client_account["id"]}`)}
 
       let row = table.insertRow(1)
-
-      row.href = "http://www.google.com/"
 
       let cell0 = row.insertCell(0)
       let cell1 = row.insertCell(1)
