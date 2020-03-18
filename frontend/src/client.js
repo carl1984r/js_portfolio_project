88f5f4d8042f62020c5a43d81d4800477538dbd5
carl1984r <carl.cc@gmail.com>
2020-03-18 05:26:45 -0500

    Created buildClient(), cleaned up renderClients.

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -1,26 +1,33 @@
+const td = document.querySelector('div.item.sidebar table#table1 tr td')
+const caption = document.querySelector('div.item.sidebar table#table1 caption')
+const hr = document.createElement('hr')
+
 class Client {
   constructor(name, id) {
     this.name = name
     this.id = id
   }
-}
 
-function renderClients(json) {
-  const td = document.querySelector('div.item.sidebar table#table1 tr td')
-  const caption = document.querySelector('div.item.sidebar table#table1 caption')
-  const hr = document.createElement('hr')
-
-  td.className = 'fade-in'
-
-  caption.appendChild(hr)
-  json.data.forEach(client => {
-    let a_client = new Client(client["attributes"]["name"], client["id"])
-    let name = document.createTextNode(`${a_client.name}`)
+  buildClient() {
+    let data = [this.name, this.id]
+    let name = document.createTextNode(`${data[0]}`)
     let a = document.createElement('a')
-        a.onclick = function() {fetchClientAccounts(`${a_client.id}`)}
+        a.onclick = function() {fetchClientAccounts(`${data[1]}`)}
         a.appendChild(name)
     let li = document.createElement('li')
         li.appendChild(a)
         td.appendChild(li)
+  }
+}
+
+function renderClients(json) {
+
+  td.className = 'fade-in'
+
+  caption.appendChild(hr)
+  
+  json.data.forEach(client => {
+    let a_client = new Client(client["attributes"]["name"], client["id"])
+        a_client.buildClient()
   })
 }
